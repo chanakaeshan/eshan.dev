7ba2d2ac088375f9ea7a191561dbb1e7dd13e47c
Chanaka Eshan <chanueshan291@gmail.com>
2025-08-01 17:47:29 +0530

    Reduce animation duration in TechStack component

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -23,7 +23,7 @@ const TechStack = () => {
         transition={{
           opacity: { duration: 0.5 },
           x: {
-            duration: 15, // Reduced from 25 to 15 seconds
+            duration: 15,
             repeat: Infinity,
             ease: "linear",
             delay: 0.5
