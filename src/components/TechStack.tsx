fe3678bebc2ab3965ba9bdf8e2c7b72792a91300
Chanaka Eshan <chanueshan291@gmail.com>
2025-08-13 07:09:55 +0530

    Comment out unused logo in TechStack component for cleaner code

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -6,7 +6,7 @@ const TechStack = () => {
     "/lovable-uploads/bb50362c-6879-4868-bbc9-c6e051fd8d7d.png",
     "/lovable-uploads/1e2a48dc-059b-4919-a1ed-44685d771a32.png",
     "/lovable-uploads/bf56a0c6-48e4-49f7-b286-8e3fda9a3385.png",
-    "/lovable-uploads/7cc724d4-3e14-4e7c-9e7a-8d613fde54d0.png",
+    // "/lovable-uploads/7cc724d4-3e14-4e7c-9e7a-8d613fde54d0.png",
   ];
 
   const extendedLogos = [...logos, ...logos, ...logos];
